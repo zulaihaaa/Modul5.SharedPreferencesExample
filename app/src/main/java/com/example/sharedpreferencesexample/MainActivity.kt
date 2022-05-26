2ee3854cdc11ce0d0c27f7403d1cea2cfddcdda6
Siti Zulaiha <rupatsiti12@gmail.com>
2022-05-26 12:50:48 +0700

    Shared Preferences Example

---
+++ app/src/main/java/com/example/sharedpreferencesexample/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.sharedpreferencesexample
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val pref = getPreferences(Context.MODE_PRIVATE)
+        btnsave.setOnClickListener {
+            val editor = pref.edit()
+            editor.putString("lastname", txtlastname.text.toString())
+            editor.putString("firstname", txtfirstname.text.toString())
+            editor.apply()
+            Toast.makeText(this, "Saved data", Toast.LENGTH_LONG).show()
+        }
+        btnload.setOnClickListener {
+            val mlastname = pref.getString("lastname", "")
+            val mfirstname = pref.getString("firstname", "")
+            val moutput = "$mfirstname $mlastname"
+            txtoutput.text = moutput
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        txtfirstname.setText("")
+        txtlastname.setText("")
+        txtfirstname.setHint("first name")
+        txtlastname.setHint("last name")
+        txtoutput.setText("")
+    }
+}
